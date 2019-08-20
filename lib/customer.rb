eefa654c122a4da6783e084de405ea00eabc2eff
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-20 01:22:22 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
+require "pry"
 class Customer
+  
+  attr_accessor :name, :age
+  
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+    
+  def self.all
+    @@all
+  end
+    
+  def new_meal(a_waiter, a_total, a_tip)
+    a_waiter = self.new(name, age)
+    a_total = total
+    d_tip = tip
+  end
   
 end
