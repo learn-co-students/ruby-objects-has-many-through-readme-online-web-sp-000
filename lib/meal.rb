364780fd6f543fa4328f34e62b9bf8ea6ae303c1
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-02 01:34:37 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,34 @@
 class Meal
 
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals #In plain English, the customer is going to
+    # look at all of the meals, and then select
+    # only the ones that belong to them. Translated into code,
+    # that could be written like the following:
+    Meal.all.select do |meal|
+    meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+    end
+  end
+  
 end
