9c503af91964ba3dffd0a01959ddf9ecf1fb1731
Panela Leung <leungpanela@gmail.com>
2019-09-17 22:11:31 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -9,7 +9,23 @@ class Customer
     @@all << self
   end
 
+  def new_meal(waiter, total, tip =0)
+    Meal.new(waiter, self, total, tip)
+  end
   def self.all
     @@all
   end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
 end
