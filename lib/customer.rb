3992aaf475d46b339369c57bc2ea9da4a95191f3
Chantel Lowe <chantellowe17@gmail.com>
2019-09-23 00:09:15 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,4 +14,20 @@ class Customer
     @@all
   end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+  
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+  
 end
