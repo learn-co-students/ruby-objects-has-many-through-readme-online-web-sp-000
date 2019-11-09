d3de19f21469994f48aba706766d3b5e227ba842
Michael Carroll <carrollm2@gmail.com>
2019-11-09 19:32:40 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Customer
-  
+  attr_accessor :name, :age, :meals
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+    @meals = []
+    @waiters = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    new_meal = Meal.new(waiter, self, total, tip)
+    @meals << new_meal
+    @waiters << new_meal.waiter
+  end
+
+  def meals
+    @meals
+  end
+
+  def waiters
+    @waiters
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Waiter
+
+  attr_accessor :name, :years_of_experience
+
+  @@all = []
+
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @@all << self
+    @meals = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    @meals
+  end
+
+  def new_meal(customer, total, tip)
+    new_meal = Meal.new(self, customer, total, tip)
+    @meals << new_meal
+    @@all << new_meal
+  end
+
+  def best_tipper
+    best_tipped_meal = @meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    best_tipped_meal.customer
+  end
 
 end
