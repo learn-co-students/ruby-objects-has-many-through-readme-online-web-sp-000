44db8803ce64fb27208227a06199f797057a9787
github username <github email address>
2020-01-30 11:55:31 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
 class Customer
   
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+    #returns all meal instances associated with a customer
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter 
+        end
+    end
+    # returns waiter instances associated with a customer
+
+
+
+
 end
