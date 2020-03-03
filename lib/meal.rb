b8749dd1a708d21197ccd73d6a4a9b0962664906
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-03 02:01:53 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = [].uniq
+
+  def initialize (waiter, customer, total, tip)
+  @waiter = waiter
+  @customer = customer
+  @total = total
+  @tip = tip
+  @@all << self
+  customer.meals << self
+  customer.waiters << waiter
+  waiter.meals << self
+  end
+
+  def self.all
+  @@all
+  end
 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :experience, :customers
+
+  @@all = [].uniq
+
+  def initialize (name, experience)
+  @name = name
+  @experience = experience
+  @meals = []
+  @@all << self
+  end
+
+  def new_meal(customer, total, tip)
+  meal = Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+  @meals
+  end
+
+def best_tipper
+self.meals.sort {|a,b| a.tip <=> b.tip}[-1].customer
+end
+
+  def self.all
+  @@all
+  end
 
 end
