59ec292b1acd928c631fcc2a57c07834ae0a96c9
github username <github email address>
2020-03-17 21:19:56 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name, @age = name, age
+    @@all << self
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(self, waiter, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.customer.name == self.name }
+  end
+
+  def waiters
+    waiter_array = []
+    self.meals.each do |meal| 
+      waiter_array << meal.waiter if meal.waiter
+    end
+    waiter_array
+  end
+
+  def self.all
+    @@all
+  end
 end
