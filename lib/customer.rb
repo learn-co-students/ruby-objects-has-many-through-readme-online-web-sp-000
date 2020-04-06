4997896244c29e22983db9d7aea4e173cde52789
C. Leow <chienleow1@gmail.com>
2020-04-06 16:38:42 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
   
+  attr_accessor :name, :age
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal
+  end
+  
+  def meals
+  end
+
+  def waiters
+  end
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
 
+  attr_accessor :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def new_meal
+  end
+  
+  def meals
+  end
+  
+  def best_tipper
+  end
+  
 end
