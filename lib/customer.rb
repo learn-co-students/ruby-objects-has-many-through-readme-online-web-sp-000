2c4332c7cb7f4f617052d9b45bb27d03e03c24c3
C. Leow <chienleow1@gmail.com>
2020-04-06 19:16:03 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,9 +19,15 @@ class Customer
   end
   
   def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
   end
 
   def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
   end
   
 end
