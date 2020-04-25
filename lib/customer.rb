41e3c135d33fff0e51f66fb103abe98dde308de3
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 14:28:00 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
   
+  @all = []
+  attr_accessor :name, :age
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @all
+  end
+  
 end
+
+class Waiter
+  attr_accessor :name, :yrs_experience
+  @@all = []
+  def initialize()
