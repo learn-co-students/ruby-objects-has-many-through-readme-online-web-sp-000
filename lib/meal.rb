0358e4fc130fe6b20f20179e15a824e06f0a3c01
fut0k0 <cccabell@icloud.com>
2020-11-05 13:30:21 -0800

    Passing lab

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+	attr_reader :waiter, :customer, :total, :tip
 
+	@@all = []
+
+	def self.all
+		@@all
+	end
+
+	def initialize(waiter, customer, total, tip)
+		@waiter = waiter
+		@customer = customer
+		@total = total
+		@tip = tip
+		@@all.push(self)
+	end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+	attr_accessor :name, :exp_yrs
 
+	@@all = []
+
+	def self.all
+		@@all
+	end
+
+	def initialize(name, exp_yrs)
+		@name = name
+		@exp_yrs = exp_yrs
+		@@all.push(self)
+	end
+
+	def new_meal(customer, total, tip=0)
+		Meal.new(self, customer, total, tip)
+	end
+
+	def meals
+		Meal.all.filter{|meal| meal.waiter == self}
+	end
+
+	def best_tipper
+		meals.max_by{|meal| meal.tip}.customer
+	end
 end
