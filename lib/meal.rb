d1cf79167b7367856663b81214424db5488ae528
Keith Filkins <kafilkins@me.com>
2020-12-21 03:30:00 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
+  attr_accessor :name, :experience
+
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+  end
+
+  def self.all
+    @@all
+  end
 
 end
