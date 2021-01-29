4ed5f11fb48d17ee95751a1859c798adcf4aff3a
Shami Tomita <shamitomita@gmail.com>
2021-01-29 22:29:15 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
+  attr_accessor :name, :age 
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end 
+  
+  def meals #the customer is going to look at all of the meals and then select only the ones that belong to them
+    Meal.all.select do |meal|
+      meal.customer == self 
+    end 
+  end 
+  
+  def waiters 
+    meals.map do |meal|
+      meal.waiter
+     end 
+   end 
+   
+   
   
 end
