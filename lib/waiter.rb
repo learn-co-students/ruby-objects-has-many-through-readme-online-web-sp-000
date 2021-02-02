672c135cce690e90809d60247b8cc9a2976ce637
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 18:24:21 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
 class Waiter
 
+  attr_accessor :name, :years_experience
+ 
+  @@all = []
+ 
+  def initialize(name, years_experience)
+    @name = name
+    @years_experience = years_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+
+  def best_tipper
+    best_customer = nil
+    best_tip = 0
+    best_meal = Meal.all.select do |meal|
+      if meal.tip > best_tip
+        best_tip = meal.tip
+        best_customer = meal.customer
+       end
+    end
+    return best_customer
+  end
+
 end
