e698cba828f029428636f1b8d484b10249993e45
Chris Baum <christopher.baum@valpo.edu>
2021-02-04 03:15:34 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,4 +13,29 @@ class Waiter
     @@all
   end
 
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    @meals = Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+
+  def best_tipper
+    largest_tip = 0
+    customer_with_largest_tip = ""
+    meals.each do |meal|
+      #binding.pry
+      if meal.tip > largest_tip
+        largest_tip = meal.tip
+        customer_with_largest_tip = meal.customer
+      end
+      #binding.pry
+    end
+    customer_with_largest_tip
+  end
+
+
 end
