a2d012ae143a8d675b337aaa0e21b2f555011de9
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-27 20:42:54 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
-  
+   attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all 
+    @@all
+  end
+ 
+ def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+    # we don't need to take customer in as an argument, because we're passing in self as a reference
+  end
 end
