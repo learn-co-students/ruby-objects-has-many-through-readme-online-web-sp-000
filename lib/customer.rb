10c5e2d4e2d64ec7d67a0a7b74575958692850a4
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-04 20:11:56 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,50 +1,50 @@
-class Customer
+
+  class Customer
+    
   attr_accessor :name, :age
- 
+
   @@all = []
- 
+
   def initialize(name, age)
     @name = name
     @age = age
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
- 
+
+ # OBJECT RELATIONSHIPS - #new_meal, #meals, #waiters
+
+ # Returns an Array of Meal instances associated with this customer
   def meals
     Meal.all.select do |meal|
       meal.customer == self
     end
   end
- 
+
+ # Returns an Array of Waiter instances associated w/ this customer's meals
   def waiters
     meals.map do |meal|
       meal.waiter
     end
   end
- 
+
+  # Initializes a meal using the current Customer instance, 
+    # a provided Waiter instance and a total and tip
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
- 
+
   def new_meal_20_percent(waiter, total)
     tip = total * 0.2
     Meal.new(waiter, self, total, tip)
   end
- 
- def best_tipper
-  best_tipped_meal = meals.max do |meal_a, meal_b|
-    meal_a.tip <=> meal_b.tip
-  end
- 
-  best_tipped_meal.customer
-end
 
-  def self.oldest_customer
+  def self.oldest_customer # Class method to find the odlest customer
     oldest_age = 0
-    oldest_customer = nil
+    oldest_customer = nil #not assigned yet 
     self.all.each do |customer|
       if customer.age > oldest_age
         oldest_age = customer.age
@@ -53,5 +53,5 @@ end
     end
     oldest_customer
   end
- 
-end
+
+end 
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,9 +1,9 @@
-class Meal
- 
+  class Meal
+    
   attr_accessor :waiter, :customer, :total, :tip
- 
+
   @@all = []
- 
+
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
@@ -11,8 +11,8 @@ class Meal
     @tip = tip
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
-end
+end 
