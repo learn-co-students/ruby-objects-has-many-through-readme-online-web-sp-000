eaad48ac1496967fc80da8f65c23d67644c868e1
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 23:30:58 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Customer
-  
+  attr_accessor :name, :age, :waiter, :total, :tip, :cutomer, :years, :meal
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+ def new_meal(waiter, total, tip)
+Meal.new(waiter, self, total, tip)
+ end
+def meals
+ Meal.all.select {|meal| meal.customer == self}
+ end
+#########
+ def waiters
+   #Waiter.all.select {|waiter|waiter.customer  == self.meal}
+meals.map do |meal| meal.waiter end
+ end
+
+########
 end
