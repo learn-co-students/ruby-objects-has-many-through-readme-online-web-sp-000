ce69bdfccb939e0435cd49fee63ab29e4b309bca
dotnotation <dotinmotionproductions@gmail.com>
2021-05-30 13:44:38 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+    # customer has many meals and waiters, meals are the joining factor between customer and waiter
+    attr_accessor :waiter, :customer, :total, :tip
+ 
+    @@all = []
+ 
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
+    # has many meals, has many customers through meals
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals #way to access all the meals the waiter has served
+        Meal.all.select do |meal|
+          meal.waiter == self #checking for  current waiter
+        end
+    end
+
+    def best_tipper #sort customer tips from highest to lowest
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip #gets the highest tip
+        end
+       
+        best_tipped_meal.customer #associates the tip with the customer and returns that customer
+    end
 
 end
